b542cb342b24f78d59d19c9a62e52c838b83b6e0
rohit <rohit.rew@gmail.com>
2022-12-11 13:05:47 +0530

    remove function added

--- main.cpp
+++ main.cpp
@@ -48,6 +48,56 @@ public:
         return NewNode->data;
     }
 
+    int addAt(int val , int index){
+        Node* NewNode = new Node();
+        NewNode->data = val;
+        int count = 0;
+
+        Node* currentNode = head;
+        while (count != index-1)
+        {
+            currentNode = currentNode->next;
+            count++;
+        }
+        
+        NewNode->next = currentNode->next;
+        currentNode->next = NewNode;
+        return NewNode->data;
+    }
+
+    int removeTop(){
+        Node* currentNode = head;
+        while (currentNode != head)
+        {
+            currentNode = currentNode->next;
+        }
+        
+        int removedVal = head->data;
+        head = head->next;
+        currentNode->next = head;
+        return removedVal;
+    }
+
+    int removeBottom(){
+        Node* currentNode = head;
+        // Node* prevNode;
+        while (currentNode->next->next != head)
+        {
+            currentNode = currentNode->next;
+            // prevNode = currentNode;
+        }
+
+        // prevNode->next = head;
+        int currentVal = currentNode->next->next->data;
+        currentNode->next->next = NULL;
+        currentNode->next = head;
+        return currentVal;
+    }
+
+    int removeAt(){
+        return 0;
+    }
+
     
     void print(){
         Node* currentNode = head;
@@ -75,7 +125,17 @@ public:
         cout << currentNode->data << endl;
         cout << "print loop ends" << endl;
     }
-    
+
+    int size(){
+        Node* currentNode = head;
+        int count = 0;
+        while (currentNode->next != head)
+        {
+            currentNode = currentNode->next;
+            count++;
+        }
+        return count+1;
+    }
     
 };
 
@@ -88,8 +148,12 @@ int main(int argc, const char * argv[]) {
     CircleList1->addAtEnd(13);
     CircleList1->addAtEnd(14);
     CircleList1->addAtTop(22);
+    CircleList1->addAt(77,3);
+
+    // CircleList1->removeTop();
+    cout << "removed : "  << CircleList1->removeBottom() << endl;
     CircleList1->print();
     // CircleList1->printLoop(20);
     
-    cout << "number : " << CircleList1->head->next->next->next->next->next->next->next->next->next->next->data <<endl;
+    cout << "size of list : " << CircleList1->size() <<endl;
 }
